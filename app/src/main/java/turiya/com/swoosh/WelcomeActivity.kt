0f83c918096390235e2fdafd4ae3594bee6b4d0e
ShivamKSingh <shivamk522@gmail.com>
2019-01-03 11:33:31 +0530

    SkillSelectionLayout Added

--- app/src/main/java/turiya/com/swoosh/WelcomeActivity.kt
+++ app/src/main/java/turiya/com/swoosh/WelcomeActivity.kt
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_welcome.*
 
-class WelcomeActivity : AppCompatActivity() {
+class WelcomeActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
